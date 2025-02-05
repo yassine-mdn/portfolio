4c719ca16ccc4d0be6b5db590f56a78a4f0cca7e
yassine-mdn <yassinemouddene@gmail.com>
2025-02-05 13:24:32 +0100

    fix: Badge Component

---
+++ src/components/Badge.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+type Props = {
+    icon?: React.FC<React.SVGProps<SVGSVGElement>>,
+    children: string
+}
+const Badge = (props: Props) => {
+    return (
+        <span className={"bg-neutral-600/20 rounded-md py-0.5 px-1.5 w-fit leading-none inline-flex items-center gap-0.5 align-middle"}>
+            {props.icon && (
+                    <props.icon className={"block rounded-sm"}/>
+            )}
+            <span className={"text-neutral-400 text-sm font-light "}>{props.children}</span>
+        </span>
+    );
+};
+
+export default Badge;
