368e5def6c828a1dee3cac77bec17b78de123cf6
yassine-mdn <yassinemouddene@gmail.com>
2025-02-06 17:08:24 +0100

    feat: Added tooltips

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -4,6 +4,12 @@ import * as React from "react"
 import {useTheme} from "next-themes"
 import {animate, motion, useMotionValue} from "framer-motion"
 import {useEffect} from "react";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/Tooltip"
 
 const properties = {
     dark: {
@@ -56,43 +62,53 @@ export function ThemeToggle() {
 
 
     return (
+        <TooltipProvider>
+            <Tooltip>
+                <TooltipTrigger>
+                    <button className={"hover:text-foreground"}
+                            onClick={() => setTheme(theme === "system" || theme === "dark" ? "light" : "dark")}>
+                        <motion.svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            style={{rotate}}
+                            transition={properties.transition}
+                        >
+                            <motion.mask id={"mask"}>
+                                <rect x="0" y="0" width="100%" height="100%" fill="white"/>
+                                <motion.circle cx="12" cy="4" r={innerCircleRadius} stroke="black" fill="black"/>
+                            </motion.mask>
+                            <motion.circle stroke="currentColor" cx="12" cy="12" r={r} mask="url(#mask)"
+                                           opacity={opacity}/>
+                            <motion.g style={{scale: moonScale}} opacity={moonOpacity}>
+                                <motion.path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"/>
+                            </motion.g>
+                            <motion.g stroke="currentColor" style={{opacity}}>
+                                <motion.path d="M12 2v2"/>
+                                <motion.path d="M12 20v2"/>
+                                <motion.path d="m4.93 4.93 1.41 1.41"/>
+                                <motion.path d="m17.66 17.66 1.41 1.41"/>
+                                <motion.path d="M2 12h2"/>
+                                <motion.path d="M20 12h2"/>
+                                <motion.path d="m6.34 17.66-1.41 1.41"/>
+                                <motion.path d="m19.07 4.93-1.41 1.41"/>
+                            </motion.g>
+                        </motion.svg>
+                        <span className="sr-only">Toggle theme</span>
+                    </button>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <span>change theme</span>
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
 
-        <button className={"hover:text-foreground"}
-                onClick={() => setTheme(theme === "system" || theme === "dark" ? "light" : "dark")}>
-            <motion.svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                style={{rotate}}
-                transition={properties.transition}
-            >
-                <motion.mask id={"mask"}>
-                    <rect x="0" y="0" width="100%" height="100%" fill="white"/>
-                    <motion.circle cx="12" cy="4" r={innerCircleRadius} stroke="black" fill="black"/>
-                </motion.mask>
-                <motion.circle stroke="currentColor" cx="12" cy="12" r={r} mask="url(#mask)" opacity={opacity}/>
-                <motion.g  style={{scale: moonScale}}  opacity={moonOpacity}>
-                    <motion.path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"/>
-                </motion.g>
-                <motion.g stroke="currentColor" style={{opacity}}>
-                    <motion.path d="M12 2v2"/>
-                    <motion.path d="M12 20v2"/>
-                    <motion.path d="m4.93 4.93 1.41 1.41"/>
-                    <motion.path d="m17.66 17.66 1.41 1.41"/>
-                    <motion.path d="M2 12h2"/>
-                    <motion.path d="M20 12h2"/>
-                    <motion.path d="m6.34 17.66-1.41 1.41"/>
-                    <motion.path d="m19.07 4.93-1.41 1.41"/>
-                </motion.g>
-            </motion.svg>
-            <span className="sr-only">Toggle theme</span>
-        </button>
 
     )
 }
