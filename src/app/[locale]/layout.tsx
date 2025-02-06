6cdbf0bcbaea061c8f72f1e26bc1b239f74372bd
yassine-mdn <yassinemouddene@gmail.com>
2025-02-06 16:48:41 +0100

    feat: theme toggle animation

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -43,7 +43,7 @@ export default async function RootLayout(props: Props) {
       >
       <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
       >
