b9f112706104d8d52839e73bdc90d76bdb7b3af7
yassine-mdn <yassinemouddene@gmail.com>
2025-02-08 18:20:01 +0100

    feat: md rendering style

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,13 @@
 import Layout from "@/components/Layout";
 import AboutSection from "@/components/AboutSection";
+import ExperienceSection from "@/components/ExperienceSection";
 
 export default function Home() {
   return (
       <Layout location={"home"}>
           <div className="flex flex-col justify-between gap-4 pt-24 lg:w-[52%] lg:py-24">
               <AboutSection/>
-              <div id={"experience"} className={"h-screen w-full bg-green-300"}>zone 2</div>
+              <ExperienceSection/>
               <div id={"projects"} className={"h-screen w-full bg-blue-300"}>zone 3</div>
           </div>
       </Layout>
