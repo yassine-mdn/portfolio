f143dcda1008429b67919602920af94bec892c3a
yassine-mdn <yassinemouddene@gmail.com>
2025-02-11 16:36:36 +0100

    feat: updated README.md

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,8 @@ import Link from "next/link";
 
 type Props = {
     links: NavLinkProps[];
+    jobTitle: string;
+    subTitle: string;
     children: React.ReactNode;
 }
 
@@ -15,9 +17,8 @@ const Header = (props: Props) => {
             className={"lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-[48%] lg:flex-col lg:justify-between lg:py-24 cursor-pointer"}>
             <div>
                 <Link href={"/"} className={"text-4xl font-bold tracking-tight text-foreground sm:text-5xl cursor-pointer"}>Yassine Mouddene</Link>
-                <h2 className={"mt-3 text-lg font-medium tracking-tight text-foreground sm:text-xl"}>FullStack Software
-                    Engineer</h2>
-                <p className={"mt-4 max-w-xs leading-normal"}>Something something something</p>
+                <h2 className={"mt-3 text-lg font-medium tracking-tight text-foreground sm:text-xl"}>{props.jobTitle}</h2>
+                <p className={"mt-4 max-w-xs leading-normal"}>{props.subTitle}</p>
                 <Nav links={props.links}/>
             </div>
             {props.children}
