7d00102b905ff63beb52a3a9df980c432513c4bf
yassine-mdn <yassinemouddene@gmail.com>
2025-02-11 22:23:00 +0100

    feat: view resume button

---
+++ src/components/ViewResumeButton.tsx
@@ -0,0 +1,22 @@
+type Props = {
+    locale: string;
+    text: string;
+};
+const ViewResumeButton = (props: Props) => {
+
+    const version = props.locale === "fr"?"FR":"EN"
+
+
+    return (
+        <a
+            className={"block mt-8 lg:px-4 font-semibold text-foreground leading-none tracking-tight after:content-['_↗'] after:inline-block after:ml-1 after:transition-transform after:duration-200 hover:after:animate-floatArrow"}
+            href={`/cv/CV%20-%20Yassine%20Mouddene%20(${version}).pdf`}
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            {props.text}
+        </a>
+    );
+};
+
+export default ViewResumeButton;
