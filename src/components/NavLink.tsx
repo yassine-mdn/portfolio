51b3b7f9dd3ab81f80421f6bc69521604cc34858
yassine-mdn <yassinemouddene@gmail.com>
2025-02-11 15:41:15 +0100

    feat: Navlink animation

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -39,7 +39,7 @@ const navAnimationTwo: Variants = {
     }
 }
 
-const defaultStyle = "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 h-10  hover:text-foreground";
+const defaultStyle = "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium h-8  hover:text-foreground";
 
 const NavLink: React.FC<NavLinkProps> = ({active, className, ...props}) => {
 
